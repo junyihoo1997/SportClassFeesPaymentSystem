ec0fe4137836c6e80aedeb357d0d653c4216318d
junyihoo1997 <junyi_hoo1997@hotmail.com>
2019-07-19 11:23:07 +0800

    first commit

---
+++ src/sportclassfeespaymentsystem/SportClassFeesPaymentSystem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sportclassfeespaymentsystem;
+
+/**
+ *
+ * @author Hoo Jun Yi
+ */
+public class SportClassFeesPaymentSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Login t = new Login();
+         t.setVisible(true);
+        
+         
+    }
+    
+}
